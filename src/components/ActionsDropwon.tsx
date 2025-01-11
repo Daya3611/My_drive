0536a74fec15bc8856ec591632905498a949a21a
Daya3611 <dayanand.gawade@vsit.edu.in>
2025-01-11 13:25:43 +0530

    upload error fic

--- src/components/ActionsDropwon.tsx
+++ src/components/ActionsDropwon.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-const ActionsDropwon = ({files}:{files:string}) => {
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ellipsis, EllipsisVertical, MenuIcon } from "lucide-react";
+
+const ActionsDropwon = ({ files }: { files: string }) => {
+    const [isModalOpen, setisModalOpen] = useState(false);
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
   return (
     <div>
-      s
+      <Dialog open={isModalOpen} onOpenChange={setisModalOpen}>
+        <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
+          <DropdownMenuTrigger className="shad-no-focus">
+            <EllipsisVertical/>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default ActionsDropwon
+export default ActionsDropwon;
